2c28d83d585daeb120abcf70354004e3f9c7d241
coder-eb <ebranbright@gmail.com>
2023-06-21 21:35:47 +0530

    In-memory CRUD operations

--- src/main/java/dev/ebran/contentcalendar/controller/ContentController.java
+++ src/main/java/dev/ebran/contentcalendar/controller/ContentController.java
@@ -2,11 +2,12 @@ package dev.ebran.contentcalendar.controller;
 
 import dev.ebran.contentcalendar.model.Content;
 import dev.ebran.contentcalendar.repository.ContentCollectionRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
@@ -20,5 +21,31 @@ public class ContentController {
     @GetMapping("")
     public List<Content> findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id) {
+        if(!repository.existsById(id)) {
+               throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        repository.delete(id);
     }
 }
